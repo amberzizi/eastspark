98d428ff6b19678896f34e369034c0e993381d30
huyanmei <huyanmei@163.com>
2015-04-15 16:16:47 +0800

    更改了 控制面板首页  的功能展示

--- application/views/login/control_board.php
+++ application/views/login/control_board.php
@@ -1,15 +1,317 @@
+<style type="text/css">
+#price{
+    padding-bottom: 15px;
+    font-size:15px;
+    font-family:SimHei;
+}
+</style>
+
+<!--报价信息陈列-->
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <p id="price">目前报价设定为：<b>23USD</b>  备注：这是一个测试性的报价。         <a class="btn btn-danger btn-xs">更改报价</a>  <a class="btn btn-danger btn-xs">查看历史报价</a></p>
+        </div>
+        
+    </div>
+
+</div>
+
+<!--留言及计划-->
 <div class="container">
     <div class="row">
         <div class="col-md-6">
-            后台控制面板首页  陈列信息
+            <div class="panel panel-info">
+                <div class="panel-heading ">
+                    系统留言 <a href="#" class="btn btn-info pull-right btn-xs">查看更多</a>
+                </div>
+                <div class="panel-body">
+                    <table class="table ">
+                            <tr>
+                                <th>来自</th>
+                                <th>留言内容</th>
+                                <th>日期</th>
+                                <th>确认</th>
+                            </tr>
+                                <td>王总</td>
+                                <td>记得把单据电子版传送给客户。</td>
+                                <td>2015-01-21 15:24:23</td>
+                                <td><span class="glyphicon glyphicon-envelope" style="color: rgb(255, 47, 60);"></span></td>
+                            </tr>
+                            </tr>
+                                <td>王总</td>
+                                <td>记得把单据电子版传送给客户。</td>
+                                <td>2015-01-21 15:24:23</td>
+                                <td><span class="glyphicon glyphicon-envelope" style="color: rgb(255, 47, 60);"></span></td>
+                            </tr>
+                            </tr>
+                                <td>王总</td>
+                                <td>记得把单据电子版传送给客户。</td>
+                                <td>2015-01-21 15:24:23</td>
+                                <td><span class="glyphicon glyphicon-ok" style="color: rgb(75, 96, 146);"></span></td>
+                            </tr>
+                        </table>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="panel panel-info">
+                <div class="panel-heading ">
+                    今日计划 <a href="#" class="btn btn-info pull-right btn-xs">查看更多</a>
+                </div>
+                <div class="panel-body">
+                    <table class="table ">
+                            <tr>
+                                <th>计划内容</th>
+                                <th>日期</th>
+                            </tr>
+                                <td>今日计划去塘沽。...</td>
+                                <td>2015-01-21 15:24:23</td>
+                            </tr>
+                            </tr>
+                                <td>今日计划去。...</td>
+                                <td>2015-01-21 15:24:23</td>
+                            </tr>
+                            </tr>
+                                <td>今日计划去塘...</td>
+                                <td>2015-01-21 15:24:23</td>
+                            </tr>
+                            
+                        
+                        </table>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+
+</div>
+
+<!--运单信息陈列-->
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="panel panel-info">
+                <div class="panel-heading ">
+                    目前正在进行的<b>集装箱</b>运单   <a href="#" class="btn btn-info pull-right btn-xs">查看更多</a>
+                </div>
+                <div class="panel-body">
+                    <table class="table ">
+                        <tr>
+                            <th>出货编号</th>
+                            <th>负责人</th>
+                            <th>进行状态</th>
+                            <th>最后更新时间</th>
+                        </tr>
+                        <tr>
+                            <td>ES1COUT</td>
+                            <td>李楠</td>
+                            <td>集港上船</td>
+                            <td>2015-01-21 15:24:23</td>
+                        </tr>
+                        <tr>
+                            <td>ES1COUT</td>
+                            <td>李楠</td>
+                            <td>集港上船</td>
+                            <td>2015-01-21 15:24:23</td>
+                        </tr>
+                        <tr>
+                            <td>ES1COUT</td>
+                            <td>李楠</td>
+                            <td>集港上船</td>
+                            <td>2015-01-21 15:24:23</td>
+                        </tr>
+                        <tr>
+                            <td>ES1COUT</td>
+                            <td>李楠</td>
+                            <td>集港上船</td>
+                            <td>2015-01-21 15:24:23</td>
+                        </tr>
+                        <tr>
+                            <td>ES1COUT</td>
+                            <td>李楠</td>
+                            <td>集港上船</td>
+                            <td>2015-01-21 15:24:23</td>
+                        </tr>
+                    
+                    </table>
+                    
+                </div>
+            </div>
         
         </div>
         <div class="col-md-6">
-            后台控制面板首页  陈列信息
-        
+            
+            <div class="panel panel-info">
+                <div class="panel-heading ">
+                    目前正在进行的<b>散货船</b>运单   <a href="#" class="btn btn-info pull-right btn-xs">查看更多</a>
+                </div>
+                <div class="panel-body">
+                    <table class="table ">
+                        <tr>
+                            <th>出货编号</th>
+                            <th>负责人</th>
+                            <th>进行状态</th>
+                            <th>最后更新时间</th>
+                        </tr>
+                        <tr>
+                            <td>ES1COUT</td>
+                            <td>李楠</td>
+                            <td>集港上船</td>
+                            <td>2015-01-21 15:24:23</td>
+                        </tr>
+                        <tr>
+                            <td>ES1COUT</td>
+                            <td>李楠</td>
+                            <td>集港上船</td>
+                            <td>2015-01-21 15:24:23</td>
+                        </tr>
+                        <tr>
+                            <td>ES1COUT</td>
+                            <td>李楠</td>
+                            <td>集港上船</td>
+                            <td>2015-01-21 15:24:23</td>
+                        </tr>
+                        <tr>
+                            <td>ES1COUT</td>
+                            <td>李楠</td>
+                            <td>集港上船</td>
+                            <td>2015-01-21 15:24:23</td>
+                        </tr>
+                        <tr>
+                            <td>ES1COUT</td>
+                            <td>李楠</td>
+                            <td>集港上船</td>
+                            <td>2015-01-21 15:24:23</td>
+                        </tr>
+                    
+                    </table>
+                    
+                </div>
+            </div>
         </div>
     </div>
 
 </div>
+<!--已完成 运单信息陈列-->
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="panel panel-primary">
+                <div class="panel-heading ">
+                    已完成的<b>集装箱</b>运单   <a href="#" class="btn btn-primary pull-right btn-xs">查看更多</a>
+                </div>
+                <div class="panel-body">
+                    <table class="table ">
+                        <tr>
+                            <th>出货编号</th>
+                            <th>负责人</th>
+                            <th>关税利润</th>
+                            <th>海运费利润</th>
+                            <th>最后更新时间</th>
+                        </tr>
+                        <tr>
+                            <td>ES1COUT</td>
+                            <td>李楠</td>
+                            <td>2342</td>
+                            <td>2342</td>
+                            <td>2015-01-21 15:24:23</td>
+                        </tr>
+                        <tr>
+                            <td>ES1COUT</td>
+                            <td>李楠</td>
+                            <td>2342</td>
+                            <td>2342</td>
+                            <td>2015-01-21 15:24:23</td>
+                        </tr>
+                        <tr>
+                            <td>ES1COUT</td>
+                            <td>李楠</td>
+                            <td>2342</td>
+                            <td>2342</td>
+                            <td>2015-01-21 15:24:23</td>
+                        </tr>
+                        <tr>
+                            <td>ES1COUT</td>
+                            <td>李楠</td>
+                            <td>2342</td>
+                            <td>2342</td>
+                            <td>2015-01-21 15:24:23</td>
+                        </tr>
+                        <tr>
+                            <td>ES1COUT</td>
+                            <td>李楠</td>
+                            <td>2342</td>
+                            <td>2342</td>
+                            <td>2015-01-21 15:24:23</td>
+                        </tr>
+                    
+                    </table>
+                    
+                </div>
+            </div>
+        
+        </div>
+        <div class="col-md-6">
+            
+            <div class="panel panel-primary">
+                <div class="panel-heading ">
+                    已完成的<b>散货船</b>运单   <a href="#" class="btn btn-primary pull-right btn-xs">查看更多</a>
+                </div>
+                <div class="panel-body">
+                    <table class="table ">
+                        <tr>
+                            <th>出货编号</th>
+                            <th>负责人</th>
+                            <th>关税利润</th>
+                            <th>海运费利润</th>
+                            <th>最后更新时间</th>
+                        </tr>
+                        <tr>
+                            <td>ES1COUT</td>
+                            <td>李楠</td>
+                            <td>2342</td>
+                            <td>2342</td>
+                            <td>2015-01-21 15:24:23</td>
+                        </tr>
+                        <tr>
+                            <td>ES1COUT</td>
+                            <td>李楠</td>
+                            <td>2342</td>
+                            <td>2342</td>
+                            <td>2015-01-21 15:24:23</td>
+                        </tr>
+                        <tr>
+                            <td>ES1COUT</td>
+                            <td>李楠</td>
+                            <td>2342</td>
+                            <td>2342</td>
+                            <td>2015-01-21 15:24:23</td>
+                        </tr>
+                        <tr>
+                            <td>ES1COUT</td>
+                            <td>李楠</td>
+                            <td>2342</td>
+                            <td>2342</td>
+                            <td>2015-01-21 15:24:23</td>
+                        </tr>
+                        <tr>
+                            <td>ES1COUT</td>
+                            <td>李楠</td>
+                            <td>2342</td>
+                            <td>2342</td>
+                            <td>2015-01-21 15:24:23</td>
+                        </tr>
+                    
+                    </table>
+                    
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
 </body>
 </html>
